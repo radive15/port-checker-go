500b30ed0bda983b8f2f5ceb2bcdb8150cf83c9a
rifki <rrdvan15@gmail.com>
2026-04-21 19:30:53 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rifki/port-checker-go
+
+go 1.25.2
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+var portsToCheck = []int{
+	22,   // SSH
+	80,   // HTTP
+	443,  // HTTPS
+	3306, // MySQL
+	5432, // PostgreSQL
+	6379, // Redis
+}
