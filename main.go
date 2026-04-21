c629a717db5cd19ed71087c2f061c2ecd1e0d56b
rifki <rrdvan15@gmail.com>
2026-04-21 19:57:59 +0700

    membuat app portcheck

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
 var portsToCheck = []int{
 	22,   // SSH
 	80,   // HTTP
@@ -7,4 +14,37 @@ var portsToCheck = []int{
 	3306, // MySQL
 	5432, // PostgreSQL
 	6379, // Redis
+}
+
+func checkPort(host string, port int) bool {
+	addr := fmt.Sprintf("%s:%d", host, port)
+	conn, err := net.DialTimeout("tcp", addr, 2*time.Second)
+	if err != nil {
+		return false
+	}
+	conn.Close()
+	return true
+}
+
+func main() {
+	// ambil host dari argument pertama
+	host := "localhost" // default jika tidak ada argument
+	if len(os.Args) > 1 {
+		host = os.Args[1]
+	}
+
+	fmt.Printf("Scanning %s...\n\n", host)
+
+	openCount := 0
+	for _, port := range portsToCheck {
+		status := "CLOSED"
+		if checkPort(host, port) {
+			status = "OPEN  "
+			openCount++
+		}
+		fmt.Printf("Port %-6d %s\n", port, status)
+	}
+
+	fmt.Printf("\n%d port terbuka dari %d yang dicek\n",
+		openCount, len(portsToCheck))
 }
